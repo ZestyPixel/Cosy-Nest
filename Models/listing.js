b079ea0bd728a3d33a82962dca2993b52f29b6a2
Umar <umar2004.mahmood@gmail.com>
2025-12-28 19:55:59 +0530

    feat: add review functionality

--- Models/listing.js
+++ Models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+const review = require('./review.js');
 
 const listingSchema = new Schema({
     title: {
@@ -14,7 +15,19 @@ const listingSchema = new Schema({
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "review",
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await review.deleteMany({_id: {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./Models/listing");
+const Review = require("./Models/review.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require('./utils/ExpressError');
 const {listingSchema} = require('./schema.js');
+const {reviewSchema} = require('./schema.js');
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/cosyNest";
 
@@ -33,8 +35,18 @@ app.get("/", (req, res)=>{
 const validateListing = (req, res, next)=>{
     let {error} = listingSchema.validate(req.body);
     if(error){
-        let errMsg = error.details.map((el)=> el.message).join(',');
-        throw new ExpressError(400, result.error);
+        let errMsg = error.details.map((el)=> el.message).join(','); //Turns array of messages into a single string
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
+
+const validateReview = (req, res, next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(','); //Turns array of messages into a single string
+        throw new ExpressError(400, errMsg);
     }else{
         next();
     }
@@ -54,7 +66,7 @@ app.get("/listings/new", (req, res)=>{
 //Show Route
 app.get("/listings/:id", wrapAsync(async(req, res)=>{
     let {id} = req.params; //MongoDb auto gives every data an id.
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -87,6 +99,30 @@ app.delete("/listings/:id", wrapAsync(async(req, res)=>{
     let{id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+//Reviews, POST route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    console.log(newReview);
+    console.log(req.body);
+console.log(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Reviews, DELETE route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res)=>{
+    let{id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull : {reviews: reviewId}}); //This is the pull operator, what it does is it removes the value from the array which matches the value given.
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
 }));
 
 app.all(/.*/, (req, res, next)=>{ //1. This will match all routes and will throw an error for non existing routes. /.* / is a regex which matches all routes.
